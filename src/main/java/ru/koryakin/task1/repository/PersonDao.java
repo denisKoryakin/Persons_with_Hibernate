eaaedb36bc2ba38a2e481080993011460bcec3da
Denis Koryakin <singler1991@gmail.com>
2023-06-26 14:25:33 +0500

    refactoring JPA Repository with JPQL Query

--- src/main/java/ru/koryakin/task1/repository/PersonDao.java
+++ src/main/java/ru/koryakin/task1/repository/PersonDao.java
@@ -1,6 +1,7 @@
 package ru.koryakin.task1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.koryakin.task1.entity.Person;
 import ru.koryakin.task1.entity.PersonId;
@@ -11,10 +12,13 @@ import java.util.Optional;
 @Repository
 public interface PersonDao extends JpaRepository<Person, PersonId> {
 
+    @Query(value = "SELECT p FROM Person p WHERE LOWER(p.cityOfLiving) LIKE LOWER(?1)")
     List<Person> findPersonByCityOfLivingIgnoreCase(String city);
 
+    @Query(value = "SELECT p FROM Person p WHERE p.personId.age < ?1 ORDER BY p.personId.age")
     List<Person> findPersonByPersonIdAgeBeforeOrderByPersonIdAgeAsc(int age);
 
+    @Query(value = "SELECT p FROM Person p WHERE LOWER(p.personId.name) LIKE LOWER(?1) AND LOWER(p.personId.surname) LIKE LOWER(?2)")
     Optional<Person> findPersonByPersonIdNameIgnoreCaseAndPersonIdSurnameIgnoreCase(String name, String surname);
 }
 
